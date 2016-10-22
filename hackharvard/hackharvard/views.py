f56d0abc101b316d23051b8951c60ff34c9c47e1
Brian Guo <brianguosh@gmail.com>
2016-10-21 22:10:55 -0400

    started stuff

---
+++ hackharvard/hackharvard/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.utils.timezone import now
+from django.dispatch import receiver
+from django.conf import settings
+
+
+def home(request):
+    return render(request, 'home.html')
